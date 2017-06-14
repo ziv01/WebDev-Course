eeb1c51887fa385823603f3cb6ca5892f1d4f73e
ziv01 <ziv.knobler@gmail.com>
2017-06-14 08:41:08 +0300

    Add files via upload
    Adding Visual Studio Code IDE exercise

---
+++ VSC_EX2/script.js
@@ -0,0 +1,13 @@
+// Code goes here
+
+
+function openWindowState() {
+  document.getElementById("myImg").src = "openWindow.jpg";
+  document.getElementById("myImg").alt = "Än open Window";
+}
+
+function closeWindowState() {
+  document.getElementById("myImg").src = "closedWindow.jpg";
+  document.getElementById("myImg").alt = "Ä closed Window";
+  console.log(string1 + string2);
+}
