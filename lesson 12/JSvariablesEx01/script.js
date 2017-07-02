ab6bdd5bce069bdf6352995cca824b5b0f3f319f
ziv01 <ziv.knobler@gmail.com>
2017-07-02 12:49:21 +0300

    Exercise 01 Java script variables

---
+++ lesson 12/JSvariablesEx01/script.js
@@ -0,0 +1,8 @@
+// Code goes here
+
+   // define a variable and assign to it the value 3 --> //
+    var x=3;
+    console.log(x); // echo the variable x into the log //
+    var x="Ziv Knobler"; //set a new value of type string (my name) into the variable x //
+    console.log(x);	//echo the variable x into the log //
+    console.log(typeof x); // echo the type of variable x into the log //
