1f7063787bcb1c8fb00e524fec15f69c148af76c
ziv01 <ziv.knobler@gmail.com>
2017-07-04 13:29:16 +0300

    if statements
    initial commit script.js file

---
+++ lesson13/jsCheckVarsEx01/script.js
@@ -0,0 +1,13 @@
+// Code goes here
+
+var num1 = 8;
+var num2 = 6;
+
+if (num1 > num2) {
+  //echo number1 to the console.log as the bigger one
+  console.log("number1 is the bigger one!");
+} else
+{
+  //echo number2 is bigger than number1
+  console.log("number2 is the bigger one! ");
+}
