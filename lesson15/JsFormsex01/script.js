a5a0184214eef11f55200a611f61a566957dd75c
ziv01 <ziv.knobler@gmail.com>
2017-07-09 09:37:25 +0300

    initial commit
    lesson 15 exercise 01

---
+++ lesson15/JsFormsex01/script.js
@@ -0,0 +1,18 @@
+// Code goes here
+
+
+
+function whoIsTheBigger (form){
+
+var num1 = parseInt(form.firstNum.value);
+var num2 = parseInt(form.secondNum.value);
+
+//who is the biggter one.. 
+if(num1>=num2) {
+     console.log("the biggest number is: " +num1);
+}  else
+{
+    console.log("the biggest number is: " +num2);
+}
+
+}
