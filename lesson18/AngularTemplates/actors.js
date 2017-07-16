cfc6d85ff335e7a583c8d207765e7e82ab528e62
ziv01 <ziv.knobler@gmail.com>
2017-07-16 18:07:26 +0300

    initial commit of JSON

--- lesson18/AngularTemplates/actors.js
+++ lesson18/AngularTemplates/actors.js
@@ -9,13 +9,14 @@ catsApp.controller('ActorsGalleryCtrl', function($scope){
     this.infoURL = infoURL;
 
   }
-  
+
   $scope.actors = [
     new actor("טום","קרוז","https://images-na.ssl-images-amazon.com/images/M/MV5BMTk1MjM3NTU5M15BMl5BanBnXkFtZTcwMTMyMjAyMg@@._V1_.jpg","http://www.imdb.com/name/nm0000129/?ref_=nv_sr_1"),
     new actor("ג'וני","דפ","https://images-na.ssl-images-amazon.com/images/M/MV5BMTM0ODU5Nzk2OV5BMl5BanBnXkFtZTcwMzI2ODgyNQ@@._V1_SY1000_CR0,0,701,1000_AL_.jpg","http://www.imdb.com/name/nm0000136/?ref_=nv_sr_1"),
     new actor("בראד","פיט","https://images-na.ssl-images-amazon.com/images/M/MV5BMjA1MjE2MTQ2MV5BMl5BanBnXkFtZTcwMjE5MDY0Nw@@._V1_SY1000_CR0,0,665,1000_AL_.jpg","http://www.imdb.com/name/nm0000093/?ref_=nv_sr_1"),
     new actor("דנזל","וושיגטון","https://images-na.ssl-images-amazon.com/images/M/MV5BMjE5NDU2Mzc3MV5BMl5BanBnXkFtZTcwNjAwNTE5OQ@@._V1_SY1000_SX750_AL_.jpg","http://www.imdb.com/name/nm0000243/?ref_=nv_sr_1")
     ];
+
 	$scope.sortArg = "";
 	
 	    $scope.setImgBorder = function(event) {
@@ -29,4 +30,13 @@ catsApp.controller('ActorsGalleryCtrl', function($scope){
           event.currentTarget.className = currentClass;
         }
     };
+	/*	
+	$http.get('actors.json').then(function(response) {
+	return $scope.actors = response.data;
+	});
+,
+	error(function(data, status, headers, config) {
+	console.log(“שגיאה");
+	});
+	*/
 });
