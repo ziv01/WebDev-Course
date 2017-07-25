ec1734af4ee5c0566f0c30354d7fdb616c3b5750
ziv01 <ziv.knobler@gmail.com>
2017-07-25 22:43:49 +0300

    changed json file structure

--- finalProject/ActualFiles/AngularJSTableDemo/app.js
+++ finalProject/ActualFiles/AngularJSTableDemo/app.js
@@ -2,6 +2,6 @@ var app;
 
 var app = angular.module("crmApp", []);
 app.controller('TableCtrl', function($scope, $http) {
-    $http.get("http://localhost/test/customers.json")
-    .then(function (response) {$scope.names = response.data.records;});
+    $http.get("http://localhost/test/test.json")
+    .then(function (response) {$scope.names = response.data;});
 });
