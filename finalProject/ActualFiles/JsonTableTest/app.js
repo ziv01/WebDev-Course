ee135c69e13ad9a719589d9aff08e837bf8b2afd
ziv01 <ziv.knobler@gmail.com>
2017-07-26 12:03:21 +0300

    tableCtrl move to external file
    tableCtrl.js created, add to index.html and removed from app.js

--- finalProject/ActualFiles/JsonTableTest/app.js
+++ finalProject/ActualFiles/JsonTableTest/app.js
@@ -1,7 +1,3 @@
 var app;
 
 var app = angular.module("crmApp", []);
-app.controller('TableCtrl', function($scope, $http) {
-    $http.get("http://localhost/test/customer.json")
-    .then(function (response) {$scope.names = response.data;});
-});
