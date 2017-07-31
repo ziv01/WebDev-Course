2e4d3501738f1f984aca78c182cfc707edff3edc
ziv01 <ziv.knobler@gmail.com>
2017-07-31 18:32:53 +0300

    bug fix: module inventory, slect all/remove all fixed

--- crmProject/app/inventoryTables/inventoryTableCtrl.js
+++ crmProject/app/inventoryTables/inventoryTableCtrl.js
@@ -1,4 +1,4 @@
-app.controller("InventoryTableCtrl", ['$scope', '$http', function ($scope, $http) {
+app.controller("InventoryTableCtrl", function ($scope, $http) {
         $http.get("app/inventoryTables/inventoryDetails.json")
                 .then(function (response) { $scope.inventoryDetails = response.data; });
 
@@ -18,40 +18,30 @@ app.controller("InventoryTableCtrl", ['$scope', '$http', function ($scope, $http
          };
         $scope.remove = function () {
                 var newDataList = [];
-                $scope.selectedAll = false;
-                angular.forEach($scope.inventoryDetails, function (checked) {
-                        console.log(checked);
-                        if (!checked.checked) {
-                                  newDataList.push(checked); 
-                       }
-                });
-                var origListLength=$scope.inventoryDetails.length;
-                var newListLength=newDataList.length;
-                if (newListLength < origListLength) {
-                      if (confirm("אתה עומד למחוק רשומה!")) {
-                        $scope.inventoryDetails = newDataList;
-                      }  
-                }                
+                //$scope.selectedAll = false;
+                if ($scope.selectAllInventory !== true || $scope.selectAllInventory === null) {
+                        angular.forEach($scope.inventoryDetails, function (checked) {
+                                if (!checked.checked) {
+                                        newDataList.push(checked);
+                                }
+                        });
+                        var origListLength = $scope.inventoryDetails.length;
+                        var newListLength = newDataList.length;
+                        if (newListLength < origListLength) {
+                                if (confirm("אתה עומד למחוק רשומה!")) {
+                                        $scope.inventoryDetails = newDataList;
+                                }
+                        }
+                } else {
+                        var newDataList = [];
+                        if (confirm("אתה עומד למחוק את כל הרשומות!!")) {
+                                $scope.inventoryDetails = newDataList;
+                        }
+                }
         };
         
-        $scope.checkAll = function () {
-                var newDataList = [];
-                console.log($scope.selectedAll);
-                if (confirm("אתה עומד למחוק את כל הרשומות!!")) {
-                        $scope.inventoryDetails = newDataList;
-                }
-                /* if (!$scope.selectedAll) {
-                        $scope.selectedAll = true;
-                } else {
-                        $scope.selectedAll = false;
-                }
-                angular.forEach($scope.inventoryDetails, function (inventoryDetails) {
-                        inventoryDetails.selected = $scope.selectedAll;
-                }
-                */
-        };
         $scope.sortType = 'name'; // set the default sort type
         $scope.sortReverse = false; // set the default sort order
         $scope.filterArg = ""; // Set default value for search string
         $scope.isLoggedIn = true; //if not on main page hide login option.
-}]);
+});
