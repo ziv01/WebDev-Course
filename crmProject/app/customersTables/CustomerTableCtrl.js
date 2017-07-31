7cad0a806f09ee056cafd0dedff230f3699a9fe7
ziv01 <ziv.knobler@gmail.com>
2017-07-31 18:00:02 +0300

    bug fix: module customers remove all is working

--- crmProject/app/customersTables/CustomerTableCtrl.js
+++ crmProject/app/customersTables/CustomerTableCtrl.js
@@ -1,4 +1,4 @@
-app.controller("TableCtrl", ['$scope', '$http', function ($scope, $http) {
+app.controller("TableCtrl", function ($scope, $http) {
         $http.get("app/customersTables/CustomersDetails.json")
                 .then(function (response) { $scope.personalDetails = response.data; });
 
@@ -13,43 +13,36 @@ app.controller("TableCtrl", ['$scope', '$http', function ($scope, $http) {
                         'mobile': "",
                         'company': "",
                         'companyID': ""
-                 });
-         };
+                });
+        };
+ 
         $scope.remove = function () {
                 var newDataList = [];
-                $scope.selectedAll = false;
-                angular.forEach($scope.personalDetails, function (checked) {
-                        if (!checked.checked) {
-                                  newDataList.push(checked); 
-                       }
-                });
-                var origListLength=$scope.personalDetails.length;
-                var newListLength=newDataList.length;
-                if (newListLength < origListLength) {
-                      if (confirm("אתה עומד למחוק רשומה!")) {
-                        $scope.personalDetails = newDataList;
-                      }  
-                }                
-        };
-        
-        $scope.checkAll = function () {
-                var newDataList = [];
-                console.log($scope.selectedAll);
-                if (confirm("אתה עומד למחוק את כל הרשומות!!")) {
-                        $scope.personalDetails = newDataList;
-                }
-                /* if (!$scope.selectedAll) {
-                        $scope.selectedAll = true;
+                //$scope.selectedAll = false;
+                if ($scope.selectedAllCustomers !== true || $scope.selectedAllCustomers === null) {
+                        angular.forEach($scope.personalDetails, function (checked) {
+                                if (!checked.checked) {
+                                        newDataList.push(checked);
+                                }
+                        });
+                        var origListLength = $scope.personalDetails.length;
+                        var newListLength = newDataList.length;
+                        if (newListLength < origListLength) {
+                                if (confirm("אתה עומד למחוק רשומה!")) {
+                                        $scope.personalDetails = newDataList;
+                                }
+                        }
                 } else {
-                        $scope.selectedAll = false;
+                        var newDataList = [];
+                        console.log(!$scope.selectedAllCustomers);
+                        if (confirm("אתה עומד למחוק את כל הרשומות!!")) {
+                                $scope.personalDetails = newDataList;
+                        }
                 }
-                angular.forEach($scope.personalDetails, function (personalDetails) {
-                        personalDetails.selected = $scope.selectedAll;
-                }
-                */
         };
+
         $scope.sortType = 'name'; // set the default sort type
         $scope.sortReverse = false; // set the default sort order
         $scope.filterArg = ""; // Set default value for search string
         $scope.isLoggedIn = true; //if not on main page hide login option.
-}]);
+});
